cd246bb90135a252a818fbc4fa6dda152c63a19c
joaco-valenverguen <69991957+joaco-valenverguen@users.noreply.github.com>
2022-03-06 16:04:58 -0500

    Pseudo terminado

--- C++/WriteFile.cpp
+++ C++/WriteFile.cpp
@@ -1,53 +1,46 @@
 //***********************************************
 //  archiv01.cpp
-//  Demuestra la escritura básica en archivo
-//  ©1999, Jaime Virgilio Gómez Negrete
+//  Demuestra la escritura bï¿½sica en archivo
+//  ï¿½1999, Jaime Virgilio Gï¿½mez Negrete
 //***********************************************
 
 #include <fstream>
-#include<iostream>
-#include<iomanip>
-#include<stdlib.h>
+#include <iostream>
+#include <iomanip>
+#include <stdlib.h>
 
 using namespace std;
 
+void escribir()
+{
+    ofstream archivo;
+    int numero;
+    string nombre, frase;
 
-void escribir(){
-	ofstream archivo;
-	int numero;
-	string nombre, frase;
-	
-	cout<< "digiite el nombre del archivo" << endl;
-	getline(cin, nombre);
-	
-	archivo.open(nombre.c_str(),ios::out);
-	
-	if(archivo.fail()){
-		cout<<"no se pudo abrir el archivo";
-		exit(1);
-	}
-	
-	cout << "digite el texto del archivo" << endl;
-	getline(cin, frase);
-	
-	
-	archivo << frase;
-	
-		
-	archivo.close();
+    cout << "digiite el nombre del archivo" << endl;
+    getline(cin, nombre);
+
+    archivo.open(nombre.c_str(), ios::out);
+
+    if (archivo.fail())
+    {
+        cout << "no se pudo abrir el archivo";
+        exit(1);
+    }
+
+    cout << "digite el texto del archivo" << endl;
+    getline(cin, frase);
+
+    archivo << frase << endl;
+
+    archivo.close();
 }
 
 int main()
 {
-	escribir();
-	
-	system("pause");
-	
-	
-	
-       return 0;
-} 
+    escribir();
 
+    system("pause");
 
-
-
+    return 0;
+}
