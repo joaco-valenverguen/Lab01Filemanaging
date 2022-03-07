ed1753a63ff0cabe3b542604bef46f767d9572ab
joaco-valenverguen <69991957+joaco-valenverguen@users.noreply.github.com>
2022-03-07 18:39:22 -0500

    ReadFile updated

--- Python/readFile.py
+++ Python/readFile.py
@@ -3,6 +3,4 @@ contenido = open("nombres.txt", "r")
 
 ## Recorremos y mostramos cada línea enumerada
 contador = 1
-for línea in contenido:
-    print("Línea", contador, ":", línea)
-    contador += 1
+print(contenido.read())
